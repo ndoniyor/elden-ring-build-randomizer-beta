df72fad2843e24081ce0e9c7de68d0ff27444598
ndoniyor <ndoniyor123@gmail.com>
2023-02-22 12:54:02 -0500

    fixed return to only return entire class

--- web/__init__.py
+++ web/__init__.py
@@ -24,30 +24,13 @@ def home_page():
 def display():
     p1 = player.Player()
     p1.build_type = "I"
-    if 'armor_sets' in request.form:
+    if "armor_sets" in request.form:
         p1.build_flags.append("armor_sets")
     p1.choose_all()
     print(type(p1.spells), p1.spells[0], p1.spells[0].name, file=sys.stdout)
 
     return render_template(
         "index.html",
-        starting_class=p1.starting_class.name,
-        class_link=p1.starting_class.link,
-        helm=p1.helmet.name,
-        chest=p1.chest_armor.name,
-        gauntlets=p1.gauntlets.name,
-        legs=p1.leg_armor.name,
-        helm_link=p1.helmet.link,
-        chest_link=p1.chest_armor.link,
-        gauntlets_link=p1.gauntlets.link,
-        legs_link=p1.leg_armor.link,
-        weapon=p1.weapons.name,
-        weapon_link=p1.weapons.link,
-        ash_of_war=p1.ash_of_war.name,
-        shield=p1.shield.name,
-        spirit_ash=p1.spirit_ash.name,
-        ash_of_war_link=p1.ash_of_war.link,
-        shield_link=p1.shield.link,
-        spirit_ash_link=p1.spirit_ash.link,
-        spells=p1.spells,
+        player_info=p1,
+        checked=True if "armor_sets" in request.form else False,
     )
